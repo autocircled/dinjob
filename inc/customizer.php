7f37d022aac5db0e98281dd5621b81cf3d899f1e
Moktadir Rahman <autocircled@gmail.com>
2021-02-06 12:15:32 +0600

    renamed underscore constant

--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function dinjob_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function dinjob_customize_preview_js() {
-	wp_enqueue_script( 'dinjob-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'dinjob-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), DINJOB_VERSION, true );
 }
 add_action( 'customize_preview_init', 'dinjob_customize_preview_js' );

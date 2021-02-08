bedcdaa878918380d2aa32ac318bfcd66c731aef
Moktadir Rahman <autocircled@gmail.com>
2021-02-08 08:17:19 +0600

    Day 2

--- footer.php
+++ footer.php
@@ -11,22 +11,22 @@
 
 ?>
 
-		<footer id="colophon" class="site-footer">
-			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dinjob' ) ); ?>">
-					<?php
-					/* translators: %s: CMS name, i.e. WordPress. */
-					printf( esc_html__( 'Proudly powered by %s', 'dinjob' ), 'WordPress' );
-					?>
-				</a>
-				<span class="sep"> | </span>
-					<?php
-					/* translators: 1: Theme name, 2: Theme author. */
-					printf( esc_html__( 'Theme: %1$s by %2$s.', 'dinjob' ), 'dinjob', '<a href="https://profiles.wordpress.org/autocircle/">AutoCircle</a>' );
-					?>
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
 </div><!-- #page -->
+<footer id="colophon" class="site-footer">
+	<div class="site-info wrapper">
+		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dinjob' ) ); ?>">
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf( esc_html__( 'Proudly powered by %s', 'dinjob' ), 'WordPress' );
+			?>
+		</a>
+		<span class="sep"> | </span>
+			<?php
+			/* translators: 1: Theme name, 2: Theme author. */
+			printf( esc_html__( 'Theme: %1$s by %2$s.', 'dinjob' ), 'dinjob', '<a href="https://profiles.wordpress.org/autocircle/">AutoCircle</a>' );
+			?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -23,42 +23,39 @@
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 	
-
-<div id="page" class="site">
-
-	
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'dinjob' ); ?></a>
-	<header id="masthead" class="site-header">
-		<div class="wrapper">
-			<div class="site-branding">
+<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'dinjob' ); ?></a>
+<header id="masthead" class="site-header">
+	<div class="wrapper">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
-				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
-				$dinjob_description = get_bloginfo( 'description', 'display' );
-				if ( $dinjob_description || is_customize_preview() ) :
-					?>
-					<p class="site-description"><?php echo $dinjob_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
-
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'dinjob' ); ?></button>
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
+			endif;
+			$dinjob_description = get_bloginfo( 'description', 'display' );
+			if ( $dinjob_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $dinjob_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'dinjob' ); ?></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
 					)
 				);
 				?>
-			</nav><!-- #site-navigation -->
-		</div><!-- .wrapper -->
-	</header><!-- #masthead -->
+		</nav><!-- #site-navigation -->
+	</div><!-- .wrapper -->
+</header><!-- #masthead -->
+<div id="page" class="site wrapper">
